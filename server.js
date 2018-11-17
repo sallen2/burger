3ca9b1eee9d97923efbbed128114475cb979e8b8
Stephan Allen <sallen202009@gmail.com>
2018-11-17 09:12:06 -0500

    adding server.js file

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-const express = require('express')
-const exphbr = require('express-handlebars')
+const express = require("express");
+const exphbr = require("express-handlebars");
 
+const app = express();
+
+app.engine("handlebars", exphbr({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
